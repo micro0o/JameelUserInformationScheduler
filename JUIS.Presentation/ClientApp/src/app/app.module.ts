9bdc8d2557ba4a36ab3370c8ede5595f80c0ef7a
micro0o <micro0o2011@gmail.com>
2023-09-15 14:43:27 +0300

    now using bootstrap

--- JUIS.Presentation/ClientApp/src/app/app.module.ts
+++ JUIS.Presentation/ClientApp/src/app/app.module.ts
@@ -5,16 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { HttpClientModule } from '@angular/common/http';
 import { NgIf } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
 
 @NgModule({
   declarations: [
@@ -25,16 +20,11 @@ import { NgIf } from '@angular/common';
     AppRoutingModule,
     RouterModule,
     FormsModule,
-    MatToolbarModule,
-    MatIconModule,
     HttpClientModule,
-    MatTableModule,
-    MatFormFieldModule,
-    MatInputModule,
     ReactiveFormsModule,
     NgIf,
-    MatButtonModule,
-    MatGridListModule
+    BrowserAnimationsModule,
+    TooltipModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
